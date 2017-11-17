902219571512a2217edd8c1f4bae895b483699f5
Joe Brunscheon <osumoogle@gmail.com>
2017-11-17 12:08:01 -0700

    Work on binding with heroes list.

--- my-app/src/app/hero-list/hero-list.component.ts
+++ my-app/src/app/hero-list/hero-list.component.ts
@@ -7,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HeroListComponent implements OnInit {
+  heroes: { id: number, name: string, power: string }[];
+  selectedHero;
 
   constructor() { }
 
   ngOnInit() {
+    this.heroes = [{ id: 1, name: 'Logan', power: 'self-healing' },
+              { id: 2, name: 'Rogue', power: 'flight, super human strength' },
+              { id: 3, name: 'Storm', power: 'elemental' },
+              { id: 4, name: 'Jean', power: 'telepathy' }
+            ];
   }
 
+  setHero(hero) {
+    this.selectedHero = hero;
+  }
+
+  isSelected(hero) {
+    if (!this.selectedHero) {
+      return false;
+    }
+    return hero.id === this.selectedHero.id;
+  }
 }
